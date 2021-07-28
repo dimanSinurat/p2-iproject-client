57548c3091578a3b348cf36c31fbd11e1b809637
diman19 <yadiman19@gmail.com>
2021-07-28 18:01:39 +0700

    revisin api

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,17 +1,41 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import router from '../router'
+import axios from '../apis/server'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    title : 'Tes after repair boiler plate vuex'
+    title : 'TEST',
+    isLogin : false,
+    isWrong: false,
+    username : '',
+    nationsByName : []
   },
   mutations: {
-    
+    LOGINEXE(state, data, name) {
+      state.isLogin = data
+      state.username = name
+    },
+    GET_NAME(state, data) {
+      state.nationsByName = data
+    }
   },
   actions: {
-    
+    facebook_login({commit}, payload){
+      axios({
+        method: POST,
+        url: '/users//login/facebook',
+        data : payload
+      })
+        .then(({data}) => {
+          console.log(data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
   },
   modules: {
   }
